3bf525352180d656d6b94cf92a7c6ce6b471835f
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-11-22 00:53:47 +0100

    added apps urls && include settings

--- dringo/urls.py
+++ dringo/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from dringo import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('first_patient', views.home, name="index"),
+    path('', include('patient.urls')),
+    path('', include('account.urls')),
+    path('', include('emergency.urls')),
 ]
