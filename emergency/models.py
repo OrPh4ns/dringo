8bd5d5c1cbaca60cf32615b7ed67322a29a6c551
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-11-22 23:02:28 +0100

    added hospital & emergency migration

--- emergency/models.py
+++ emergency/models.py
@@ -5,7 +5,7 @@ from hospital.models import Hospital
 
 
 class Emergency(models.Model):
-    #name = models.CharField(max_length=300, unique=True)
-    description = models.CharField(max_length=150)
+    # name = models.CharField(max_length=300, unique=True)
+    case_id = models.AutoField(primary_key=True)
     hospital = models.ForeignKey(Hospital, related_name='emergencies', on_delete=models.CASCADE)
     created_dt = models.DateTimeField(auto_now_add=True)
--- hospital/models.py
+++ hospital/models.py
@@ -5,3 +5,6 @@ class Hospital(models.Model):
     name = models.CharField(max_length=300, unique=True)
     beds_count = models.IntegerField()
     created_dt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
