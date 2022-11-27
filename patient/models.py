e7bc8e58f7d61f46b17d729d591145d8355a6273
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-11-27 17:40:21 +0100

    [py] created patient object class

--- patient/models.py
+++ patient/models.py
@@ -2,4 +2,17 @@ from django.db import models
 
 
 class Patient(models.Model):
-    pass
+    ins_nr = models.IntegerField()
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    arrival_mode = models.IntegerField()
+    pain = models.BooleanField(default=False)
+    injury = models.BooleanField(default=False)
+    mental = models.IntegerField()
+    issue = models.TextField()
+    pain_rate = models.IntegerField()
+    systole = models.IntegerField()
+    diastole = models.IntegerField()
+    heart_raet = models.IntegerField()
+    breathing_rate = models.IntegerField()
+    body_temp = models.IntegerField()
