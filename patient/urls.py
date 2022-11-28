48b2a98491201aa496bc1f074dd1d911991cf85d
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-11-28 10:51:49 +0100

    added patient object functions

--- patient/urls.py
+++ patient/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from patient import views
 
 urlpatterns = [
-    #path('page', views.get_page, name='page'),
-    #path('info', views.get_info, name='info'),
+    path('call', views.patient_call, name='patient_call'),
+    path('patient', views.get_patient, name='get_patient'),
+    path('patients', views.get_patients, name='get_patients')
+    # path('info', views.get_info, name='info'),
 ]
--- patient/views.py
+++ patient/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def patient_call(request):
+    return render(request, 'call.html')
+
+
+def get_patient(request):
+    return render(request, 'patient.html')
+
+def get_patients(request):
+    return render(request, 'patients.html')
