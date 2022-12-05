ed523f44a3535c1ae7c5470f8da6ee93db18fb2e
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-12-05 23:46:59 +0100

    added role app

---
+++ role/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+from hospital.models import Hospital
+
+
+class Role(models.Model):
+    employee = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_car = models.BooleanField(default=False)
+    hospital = models.OneToOneField(Hospital, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.hospital) + " | " + str(self.employee) + " | Wagen = " + str(self.is_car)
