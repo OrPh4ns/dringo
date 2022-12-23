4e556ca3e071dabd31772214fac5bed0907ffa19
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-12-24 00:25:59 +0100

    update

--- emergency/urls.py
+++ emergency/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('notfalls', views.get_emergs, name='notfalls'),
     path('neuer_notfall', views.new_emerg, name='neuer_notfall'),
     path('remove_emerg/<int:id>/', views.remove_emerg),
+    path('reserv_emerg/<int:id>/', views.reserv_emerg),
     # path('info', views.get_info, name='info'),
 ]
