bcae5649a1fd49a5f0aa29f0b8ddc7562b8eb4fe
Abdulrahman Othman <57464795+OrPh4ns@users.noreply.github.com>
2022-12-24 00:26:19 +0100

    created new emerg funcition

--- emergency/views.py
+++ emergency/views.py
@@ -1,8 +1,15 @@
+from django.db.models import Count
 from django.shortcuts import render, redirect
 
 from emergency.models import Emergency
 from hospital.models import Hospital
 from role.models import Role
+
+# Own Moduls
+
+import operator
+from django.db.models import Q
+from functools import reduce
 
 
 def get_emergs(request):
@@ -12,7 +19,7 @@ def get_emergs(request):
         else:
             obj = Emergency.objects.filter(hospital=Hospital.objects.filter(
                 id=Role.objects.filter(employee=request.user.id).first().hospital.id).first())
-        return render(request, 'emergs.html', {"emeges": obj})
+        return render(request, 'emergs.html', {"emeges": obj, "hospitals": Hospital.objects.all()})
     else:
         return redirect('/einloggen')
 
@@ -24,9 +31,54 @@ def remove_emerg(request, id):
     else:
         return redirect('/einloggen')
 
+
+def new_emergx(request):
+    if request.user.is_authenticated:
+        if Role.objects.filter(employee=request.user.id).first().is_car:
+            # list_ids = list(Emergency.objects.values_list('hospital_id', flat = True))
+            emgs = Emergency.objects.values_list('hospital_id', flat=True).annotate(total=Count('case_id')).order_by()
+
+            emg_ids = []
+            for emg in emgs:
+                emg_ids.append(emg)
+            hospts = []
+            for i in emg_ids:
+                hospts += Hospital.objects.filter(id=i)
+                #reserved_beds_count = Emergency.objects.filter(hospital_id=i).count()
+                print(Emergency.objects.filter(hospital_id=i).count())
+            return render(request, 'new_emerg.html',{"hospts": hospts})
+        else:
+            obj = Emergency.objects.filter(hospital=Hospital.objects.filter(
+                id=Role.objects.filter(employee=request.user.id).first().hospital.id).first())
+            print(obj.count())
+        return render(request, 'new_emerg.html', {"emeges": obj})
+    else:
+        return redirect('/einloggen')
+
 def new_emerg(request):
     if request.user.is_authenticated:
-        from django.db.models import Count
-        return render(request, 'new_emerg.html')
+        if Role.objects.filter(employee=request.user.id).first().is_car:
+
+            hospitals = Hospital.objects.all()
+            print(hospitals)
+            return render(request, 'new_emerg.html',{"hospts": hospitals})
+        else:
+            hospitals = Hospital.objects.filter(id=Role.objects.filter(employee=request.user.id).first().hospital.id)
+            return render(request, 'new_emerg.html', {"hospts": hospitals})
+    else:
+        return redirect('/einloggen')
+
+
+def reserv_emerg(request, id):
+    if request.user.is_authenticated:
+        emerg_count = Emergency.objects.filter(hospital_id=id).count()
+        print(emerg_count)
+        if emerg_count < Hospital.objects.filter(id=id).first().beds_count:
+            new_emerg = Emergency(hospital_id=id)
+            new_emerg.save()
+        else:
+            return render(request, 'new_emerg.html', {"error": "Kein Bett verfügbar"})
+
+        return redirect('/notfalls')
     else:
         return redirect('/einloggen')
